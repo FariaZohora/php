b945447f585abed408b55b1ce1f9dde19b51bf59
Faria Zohora <fariazohora31@gmail.com>
2020-03-09 09:17:32 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/custom.css">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    
+
+      <div class"container-fluid">
+      <div class="row">
+        <div class="col-sm-6">
+        <div class="left">
+        <div class="lefty">
+            <div class="smt">
+              <i></i>
+              <h4>Address</h4>
+              <p>Mada center 8th floor, 379 Hudson St, New York, NY 10018 US</p>
+            </div>
+
+            <div class="smt">
+              <i></i>
+              <h4>Lets Talk</h4>
+              <p>+1 800 1236879</p>
+            </div>
+
+            <div class="smt">
+              <i></i>
+              <h4>General Support</h4>
+              <p>contact@example.com</p>
+            </div>
+        </div>
+        </div>
+        </div>
+        <div class="col-sm-6">
+        <div class="right">
+          <form>
+            <input name="name" type="text" placeholder="Your full name"><br><br>
+            <input name="email" type="email" placeholder="Your valid email address"><br><br>
+            <input name="inquiry" type="text" placeholder="Your inquiry"><br><br>
+            <input name="button" type="submit" placeholder="SUBMIT"><br><br>
+          </form>
+        </div>
+        </div>
+      </div>
+      </div>
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
